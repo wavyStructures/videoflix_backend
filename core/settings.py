5b52cc74a32115d6031863645145934b9b108b19
wavyStructures <anja_schwab@gmx.de>
2025-08-05 09:28:04 +0200

    user_auth_app and videoflix_app

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_rq',
+    'user_auth_app',
+    'videoflix_app',
 ]
 
 MIDDLEWARE = [
---
+++ videoflix_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import VideoListView, HLSIndexView, HLSContentView
+
+urlpatterns = [
+    path('video/', VideoListView.as_view(), name='video_list'),
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8', HLSIndexView.as_view(), name='hls_index'),
+    path('video/<int:movie_id>/<str:resolution>/<str:segment>/', HLSChunkView.as_view(), name='hls_chunk'),
+]
+

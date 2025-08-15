4417605ce36a406a0aa39427693682f1098dc051
wavyStructures <anja_schwab@gmx.de>
2025-08-15 21:48:05 +0200

    checking video upload

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -7,11 +7,17 @@ import os
 @receiver(post_save, sender=Video)  
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespeichert')
-    if created:
-        print('Video wurde erstellt')
-        convert_480p(instance.video_file.path)
+    if created and instance.video_file and instance.video_file.name:
+        file_path = instance.video_file.path
+        if os.path.isfile(file_path):
+            convert_480p(file_path)
+
+        # convert_480p(instance.video_file.path)
         # instance.video_file.seek(0)
         # instance.video_file.save(instance.video_file.name, instance.video_file, save=False
+
+
+
 
 @receiver(post_delete, sender=Video)
 def auto_delete_file_on_delete(sender, instance, **kwargs):

bd72b8cf51532ae0b2ccf48b80557e16e6c169bf
Anja Schwab <anja_schwab@gmx.de>
2025-10-11 08:31:58 +0200

    Ignore database file

--- user_auth_app/serializers.py
+++ user_auth_app/serializers.py
@@ -4,8 +4,6 @@ from django.core.mail import send_mail
 
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.password_validation import validate_password
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 
 
